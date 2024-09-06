c30bf4ce88714d80106a731dcde7ab2b29b6da1c
mathiashole <mangino@fcien.edu.uy>
2024-09-06 14:54:10 -0300

    created and comments execution module

--- OE1/chop_gen.R
+++ OE1/chop_gen.R
@@ -56,34 +56,8 @@ generate_commands <- function(data) {
   return(data)
 }
 
-
 # # Función para procesar cada conjunto de argumentos
-# process_filter_seq <- function(input_file) {
-#   # Leer el archivo de entrada
-#   # data <- read_tsv(input_file, col_names = FALSE)
-#   # colnames(data) <- c("fasta_file", "gff_file", "keyword1", "keyword2") # Ajustar los nombres de las columnas según sea necesario
-
-#   # Procesar cada fila del archivo
-#   data %>%
-#     mutate(
-#       gff_basename = basename(gff_file),
-#       no_gff_basename = str_remove(gff_basename, ".{4}$"),
-#       keyword_sum = paste(keyword1, keyword2, sep = "_"),
-#       #filtred_name_gff = str_c("filtered_", keyword_sum, "_", gff_basename),
-#       filtred_name_gff = str_c("filtered_:", keyword_sum, ":_", gff_basename),
-#       # out_gscissors = str_c("out_", keyword_sum, "_", no_gff_basename, ".fasta"),
-#       out_gscissors = str_c("out_:", keyword_sum, ":_", no_gff_basename, ".fasta"),
-#       stat_fasta_feature = str_c("stat_", keyword_sum, "_", no_gff_basename, ".tsv")
-#     ) %>%
-#     rowwise() %>%
-#     mutate(
-#       filter_seq_command = str_c(FILTER_SEQ, " ", input_file),
-#       gscissors_command = str_c(GSCISSORS, " --fasta ", fasta_file, " --coordinates ", filtred_name_gff, " --format gff --output ", out_gscissors),
-#       fasta_feature_command = str_c(SEQ_A, " ", out_gscissors),
-#       distribution_command = str_c(DISTRIBUTION, " ", stat_fasta_feature)
-#       # fasta_feature_command = str_c(SEQ_A, " ", out_gscissors)
-#     ) %>%
-#     {
+# execution_module <- function(input_file) {
 #       for (i in 1:nrow(.)) {
 #         cat("Processing FILTER_SEQ: ", .$filter_seq_command[i], "\n")
 #         system(.$filter_seq_command[i])
