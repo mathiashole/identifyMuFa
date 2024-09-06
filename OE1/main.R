8258f175dcace6695a15dd4e49dedde81e6ecf68
mathiashole <mangino@fcien.edu.uy>
2024-09-06 18:12:14 -0300

    main is old chop

---
+++ OE1/main.R
@@ -0,0 +1,123 @@
+#!/usr/bin/env Rscript
+
+
+# call all scripts
+#FILTER_SEQ <- "/home/usuario/BASH/chack_gff/chack_gff.sh"
+FILTER_SEQ <- "/home/usuario/BASH/chack_gff/chack_gff_v1.sh"
+GSCISSORS <- "/home/usuario/Data_Rstudio/seqExtractor/GScissors/gscissors.pl"
+#FASTA_FEATURE <- "/home/usuario/PERL/stat_seq/stat_seq.pl"
+SEQ_A <- "/home/usuario/Data_Rstudio/chop_genome/seq_attributes/seq_attributes.R"
+DISTRIBUTION <- "/home/usuario/Data_Rstudio/statistics_of_sequence/analyze_statistics.R"
+#SEQ_A <- "/home/usuario/Data_Rstudio/statistics_of_sequence/render_quarto.R"
+
+# Load necessary libraries
+library(tidyverse)
+
+# Create output directory if it does not exist
+create_output_dir <- function(output_dir) {
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+  }
+}
+
+# Function to read input file and set column names
+read_input <- function(input_file) {
+  data <- read_tsv(input_file, col_names = FALSE)
+  colnames(data) <- c("fasta_file", "gff_file", "keyword1", "keyword2")
+  return(data)
+}
+
+# transform_data <- function(data) {
+#   # Perform metadata transformations
+#   data <- mutate(data,
+#     gff_basename = basename(gff_file),
+#     no_gff_basename = str_remove(gff_basename, ".{4}$"),
+#     keyword_sum = paste(keyword1, keyword2, sep = "_"),
+#     filtred_name_gff = str_c("filtered_:", keyword_sum, ":_", gff_basename),
+#     out_gscissors = str_c("out_:", keyword_sum, ":_", no_gff_basename, ".fasta"),
+#     stat_fasta_feature = str_c("stat_", keyword_sum, "_", no_gff_basename, ".tsv")
+#   )
+  
+#   # Returns the transformed DataFrame
+#   return(data)
+# }
+
+# Transform the data for output filenames and paths
+transform_data <- function(data, output_dir) {
+  data <- mutate(data,
+    gff_basename = basename(gff_file),
+    no_gff_basename = str_remove(gff_basename, ".{4}$"),
+    keyword_sum = paste(keyword1, keyword2, sep = "_"),
+    filtred_name_gff = file.path(output_dir, str_c("filtered_:", keyword_sum, ":_", gff_basename)),
+    out_gscissors = file.path(output_dir, str_c("out_:", keyword_sum, ":_", no_gff_basename, ".fasta")),
+    stat_fasta_feature = file.path(output_dir, str_c("stat_", keyword_sum, "_", no_gff_basename, ".tsv"))
+  )
+  # Returns the transformed DataFrame
+  return(data)
+}
+
+# Generate system commands for each step, directing output to output_directory
+generate_commands <- function(data) {
+  data <- rowwise(data) %>%
+    mutate(
+      # Pass output directory for each command to save results in the correct folder
+      filter_seq_command = str_c(FILTER_SEQ, " ", fasta_file, " > ", filtred_name_gff),
+      gscissors_command = str_c(GSCISSORS, " --fasta ", fasta_file, " --coordinates ", filtred_name_gff, " --format gff --output ", out_gscissors),
+      fasta_feature_command = str_c(SEQ_A, " ", out_gscissors),
+      distribution_command = str_c(DISTRIBUTION, " ", stat_fasta_feature)
+    )
+  
+  return(data)
+}
+
+# Function to process each set of arguments
+execution_module <- function(data) {
+      for (i in 1:nrow(data)) {
+        cat("Processing FILTER_SEQ: ", data$filter_seq_command[i], "\n")
+        system(data$filter_seq_command[i])
+        # Check if FILTER_SEQ created the expected file
+        if (!file.exists(data$filtred_name_gff[i])) {
+          cat("Error: FILTER_SEQ did not create the file", data$filtred_name_gff[i], "\n")
+          next
+        }
+
+        # # change directory from where you get the data!! DEBUGGING
+        # cat("Processing GSCISSORS: ", data$gscissors_command[i], "\n")
+        # system(data$gscissors_command[i])
+        # if (!file.exists(data$out_gscissors[i])) {
+        #   cat("Error: GSCISSORS did not create the file", data$out_gscissors[i], "\n")
+        #   next
+        # }
+        # # change directory from where you get the data!! DEBUGGING
+        # cat("Processing SEQ_A: ", data$fasta_feature_command[i], "\n")
+        # system(data$fasta_feature_command[i])
+        # # change directory from where you get the data!! DEBUGGING
+        # cat("Processing DISTRIBUTION: ", data$distribution_command[i], "\n")
+        # system(data$distribution_command[i])
+
+        # cat("Successfully processed", data$fasta_file[i], "and keywords:", data$keyword1[i], data$keyword2[i], "\n")
+      }
+}
+
+# Check the number of arguments
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("Usage: process_sequences.R <file_with_arguments.tsv>")
+}
+
+#####################
+# Execution section #
+#####################
+
+input_file <- args[1]
+output_dir <- "output_directory"  # Define your output directory
+data <- read_input(input_file)
+# Apply the transformations
+data_transformed <- transform_data(data, output_dir)
+print(data_transformed)
+# Generate the commands
+data_with_commands <- generate_commands(data_transformed)
+# Prit debugging
+print(data_with_commands)
+# Execution script
+execution_module(data_with_commands)
