602aa066538cfbef5bea397c3d70a91e2a77144b
mathiashole <mangino@fcien.edu.uy>
2024-09-22 11:27:15 -0300

    removed: one grep

--- OE1/code/filter_keywords.sh
+++ OE1/code/filter_keywords.sh
@@ -75,7 +75,7 @@ while IFS=$'\t' read -r _ file keyword1 keyword2 || [ -n "$file" ]; do
     grep "$keyword1" "$file" | grep "$keyword2" | sort | uniq > "$output_file"
 
     # Use grep to non filter the content based on keywords
-    grep "$keyword2" "$file" | grep "$keyword2" | sort | uniq > "$output_file_single_keyword"
+    grep "$keyword2" "$file" | sort | uniq > "$output_file_single_keyword"
 
     # Check if output file is not empty
     if [ -s "$output_file" ]; then
