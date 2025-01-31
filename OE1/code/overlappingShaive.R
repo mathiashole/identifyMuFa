3cfdc238d76eb6b6e06017f6fc914c0793ddadb7
mathiashole <mangino@fcien.edu.uy>
2025-01-30 22:46:11 -0300

    add: all blast

--- OE1/code/overlappingShaive.R
+++ OE1/code/overlappingShaive.R
@@ -31,6 +31,10 @@ library(dplyr)
 # blast_data <- read.delim(blast_file, header = FALSE, sep = "\t", fill = TRUE)
 
 blast_data <- read.delim("/home/mathias/study/maestria/scoville_data/blast_results/blastn_TriTrypDB-68_TcruziDm28c2018_Genome.txt", header = FALSE, sep = "\t", fill = TRUE)
+blast_data <- read.delim("/home/mathias/study/maestria/scoville_data/blast_results/blastn_TriTrypDB-68_TcruziBrazilA4_Genome.txt", header = FALSE, sep = "\t", fill = TRUE)
+blast_data <- read.delim("/home/mathias/study/maestria/scoville_data/blast_results/blastn_TriTrypDB-68_TcruziYC6_Genome.txt", header = FALSE, sep = "\t", fill = TRUE)
+blast_data <- read.delim("/home/mathias/study/maestria/scoville_data/blast_results/blastn_TcDm25_TcruziTcDm25H1_Genome.txt", header = FALSE, sep = "\t", fill = TRUE)
+
 
 # Rearrange columns V9 and V10 into V15 and V16 based on condition
 df <- blast_data %>%
