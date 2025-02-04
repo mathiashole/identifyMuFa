4da7f99f87863934a0d08e6f5167646a6fe58157
mathiashole <mangino@fcien.edu.uy>
2025-02-04 12:10:46 -0300

    add: call overlappingshaive in main script

--- OE1/main.R
+++ OE1/main.R
@@ -4,6 +4,7 @@
 # call all scripts
 FILTER_SEQ <- "code/filter_keywords.sh"
 GSCISSORS <- "code/Genomics_Scissors/gscissors.pl"
+OVERLAPPINGSHAIVE <- "code/overlappingShaive.R"
 # SEQ_A <- "/home/usuario/Data_Rstudio/chop_genome/seq_attributes/seq_attributes.R"
 # DISTRIBUTION <- "/home/usuario/Data_Rstudio/statistics_of_sequence/analyze_statistics.R"
 #SEQ_A <- "/home/usuario/Data_Rstudio/statistics_of_sequence/render_quarto.R"
