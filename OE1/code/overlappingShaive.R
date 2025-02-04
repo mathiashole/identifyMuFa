5390c2bd3c0c5111eb940806ef07ffd118c5026c
mathiashole <mangino@fcien.edu.uy>
2025-02-04 10:26:33 -0300

    add: saved data

--- OE1/code/overlappingShaive.R
+++ OE1/code/overlappingShaive.R
@@ -139,11 +139,11 @@ if (!is.null(gff_file)) {
 
 if (!is.null(table_format)) {
   if (table_format == "csv") {
-    write_csv(df_no_overlaps, "limits.csv", col_names = FALSE)
-    write_csv(filtered_data, "filtered_data.csv", col_names = FALSE)
+    write_csv(df_no_overlaps, "all_multigenic family_data.csv", col_names = FALSE)
+    write_csv(filtered_data, "filtered_multigenic family_data.csv", col_names = FALSE)
   } else if (table_format == "tsv") {
-    write_tsv(df_no_overlaps, "limits.tsv", col_names = FALSE)
-    write_tsv(filtered_data, "filtered_data.tsv", col_names = FALSE)
+    write_tsv(df_no_overlaps, "all_multigenic family_data.tsv", col_names = FALSE)
+    write_tsv(filtered_data, "filtered_multigenic family_data..tsv", col_names = FALSE)
   } else {
     stop("Unsupported table format specified.")
   }
