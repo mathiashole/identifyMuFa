44cba535b3a3d5626460456cfc984951746a8888
mathiashole <mangino@fcien.edu.uy>
2025-02-14 10:51:54 -0300

    add: comment

--- OE1/code/spDiffSize.R
+++ OE1/code/spDiffSize.R
@@ -27,16 +27,16 @@ if (!xor(!is.null(tsv_file), !is.null(gff_file))) {
   stop("Error: You must specify either --tsv or --gff, but not both.")
 }
 
-if (length(args) < 2) {
-  stop("Usage: Rscript spDiffSize.R <input_file> <threshold>")
-}
+# if (length(args) < 2) {
+#   stop("Usage: Rscript spDiffSize.R <input_file> <threshold>")
+# }
 
-input_file <- args[1]
-threshold <- as.numeric(args[2])
+# input_file <- args[1]
+# threshold <- as.numeric(args[2])
 
-if (!file.exists(input_file)) {
-  stop("Error: Input file does not exist.")
-}
+# if (!file.exists(input_file)) {
+#   stop("Error: Input file does not exist.")
+# }
 
 # Read the dataframe (assuming tab-separated, change if needed)
 df <- read.table(input_file, header = FALSE, sep = "\t")
