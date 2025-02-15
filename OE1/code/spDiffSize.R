b80edd8e63fda7b4ada7356e2dde1ffc8de59b2a
mathiashole <mangino@fcien.edu.uy>
2025-02-14 21:54:21 -0300

    debug: read gff file

--- OE1/code/spDiffSize.R
+++ OE1/code/spDiffSize.R
@@ -44,7 +44,7 @@ if (!is.null(tsv_file)) {
 } else if (!is.null(gff_file)) {  # Read GFF file
   input_file <- gff_file
   message("Processing GFF file: ", input_file)
-  df <- read.table(input_file, header = FALSE, sep = "\t", quote = "", comment.char = "#")
+  df <- read.delim(input_file, comment.char = "#", header = FALSE, sep = "\t") # read.table(input_file, header = FALSE, sep = "\t", quote = "", comment.char = "#")
 
   if (ncol(df) < 9) stop("Error: GFF must have at least 9 columns.")
 
