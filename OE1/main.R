bae3c87fa46810b17b5f689cb7d0de13d2166862
mathiashole <mangino@fcien.edu.uy>
2025-02-17 09:49:49 -0300

    removed:comments of 5 and 3 column

--- OE1/main.R
+++ OE1/main.R
@@ -29,10 +29,10 @@ read_input <- function(input_file) {
   # num_cols <- max(colSums(data != "" & !is.na(data))) 
 
   # Assign names based on the number of columns
-  if (num_cols == 4) { # if (num_cols == 5 || num_cols == 4) {
+  if (num_cols == 4) {
   # if (num_cols == 5 || num_cols == 4) {
     colnames(data) <- c("fasta_file", "gff_file", "keyword1", "keyword2")
-  } else if (num_cols == 2) { # } else if (num_cols == 3 || num_cols == 2) {
+  } else if (num_cols == 2) {
   # } else if (num_cols == 3 || num_cols == 2) {
     colnames(data) <- c("fasta_file", "sequence_file")
   } else {
