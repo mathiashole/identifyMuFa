e8197cced89fefb56f35fd4b9102c21790fb680a
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2025-02-27 12:10:07 -0300

    changed: no filtered to filtered

--- OE1/main.R
+++ OE1/main.R
@@ -65,10 +65,10 @@ transform_data <- function(data) {
   data$tblastn_result <- paste0("tblastn_", data$no_fasta_basename, ".txt") # result first PBLASTN
   data$overlappingshaive_result <- paste0("all_multigenic_family_", data$no_fasta_basename, ".tsv") # results filtered no overlap sequence
   data$overlappingshaive_result_filtered <- paste0("filtered_multigenic_family_", data$no_fasta_basename, ".tsv") # Results filtered no overlap and old sequence
-  data$overlap_result_high_equal_df <- paste0("high_equal_all_multigenic_family_", data$no_fasta_basename, ".tsv") # Split result of no overlap > or = to minimal length
-  data$overlap_result_low_df <- paste0("low_all_multigenic_family_", data$no_fasta_basename, ".tsv") # Split result of no overlap < to minimal length
-  data$overlap_result_high_equal <- paste0("high_equal_all_multigenic_family_", data$no_fasta_basename, ".fasta") # extracted sequence > or = to minimal length
-  data$overlap_result_low <- paste0("low_all_multigenic_family_", data$no_fasta_basename, ".fasta") # Extracted sequence < to minimal length
+  data$overlap_result_high_equal_df <- paste0("high_equal_filtered_multigenic_family_", data$no_fasta_basename, ".tsv") # Split result of no overlap > or = to minimal length
+  data$overlap_result_low_df <- paste0("low_filtered_multigenic_family_", data$no_fasta_basename, ".tsv") # Split result of no overlap < to minimal length
+  data$overlap_result_high_equal <- paste0("high_equal_filtered_multigenic_family_", data$no_fasta_basename, ".fasta") # extracted sequence > or = to minimal length
+  data$overlap_result_low <- paste0("low_filtered_multigenic_family_", data$no_fasta_basename, ".fasta") # Extracted sequence < to minimal length
   data$gorf_result_file <- paste0("getorf_", data$overlap_result_high_equal)
 
   return(data)
