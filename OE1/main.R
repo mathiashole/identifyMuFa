6a764d5248a17b454726bb7a1e34981396c3626b
mathiashole <mangino@fcien.edu.uy>
2025-03-19 09:22:03 -0300

    add: first transeq in command line

--- OE1/main.R
+++ OE1/main.R
@@ -120,6 +120,8 @@ generate_commands <- function(data, output_dir) {
   
   data$bothblast_command <- paste(BOTHBLAST, file.path(output_dir, data$out_gscissors_high), file.path(output_dir, "blast_result"), data$fasta_file)
   # data$allblast_first_command <- paste(ALLBLAST, "-type", "blastn", "-qn", file.path(output_dir, data$out_gscissors_high), "-sn", data$fasta_file, "-o", file.path(output_dir, "blast_result"))
+  # data$allblast_first_transeq_command <- paste(ALLBLAST, "-transeq", "-qn", file.path(output_dir, data$out_gscissors_high))
+  
   data$overlappingshaive_command <- paste("Rscript", OVERLAPPINGSHAIVE, "--blast_file", file.path(output_dir, "blast_result", data$blastn_result), 
                                           "--gff_file", file.path(output_dir, data$sp_high_filtred_name_gff), "--output_dir", output_dir, "--inter", 100) ## THIS INTER OPTION NEED ESTIMATED IN PROGRAM
   
