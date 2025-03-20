dc99414cc18f490022513a1f33cd66017618ef52
mathiashole <mangino@fcien.edu.uy>
2025-03-20 10:11:03 -0300

    add: space in command text

--- OE1/main.R
+++ OE1/main.R
@@ -124,7 +124,9 @@ generate_commands <- function(data, output_dir) {
                                        file.path(output_dir, data$out_rest_gscissors)) ###### Rest of sequence not searched ####
   
   data$bothblast_command <- paste(BOTHBLAST, file.path(output_dir, data$out_gscissors_high), file.path(output_dir, "blast_result"), data$fasta_file)
+  
   # data$allblast_first_command <- paste(ALLBLAST, "-type", "blastn", "-qn", file.path(output_dir, data$out_gscissors_high), "-sn", data$fasta_file, "-o", file.path(output_dir, "blast_result"))
+  
   # data$allblast_first_transeq_command <- paste(ALLBLAST, "-transeq", "-qn", file.path(output_dir, data$out_gscissors_high))
   
   data$overlappingshaive_command <- paste("Rscript", OVERLAPPINGSHAIVE, "--blast_file", file.path(output_dir, "blast_result", data$blastn_result), 
