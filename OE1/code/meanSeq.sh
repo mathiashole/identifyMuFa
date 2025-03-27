dc31cad33d77abab997d929d515ef71a4ee83e57
mathiashole <mangino@fcien.edu.uy>
2025-03-27 10:39:00 -0300

    add: comment gff function

--- OE1/code/meanSeq.sh
+++ OE1/code/meanSeq.sh
@@ -6,6 +6,7 @@ calculate_avg_length_fasta() {
     awk '/^>/ {if (seqlen) {sum+=seqlen; count++} seqlen=0; next} {seqlen+=length($0)} END {sum+=seqlen; count++; print int(sum/count)}' "$1"
 }
 
+# Function to calculate the average feature length from a GFF file
 calculate_avg_length_gff() {
     awk '$1 !~ /^#/ {sum += ($5 - $4 + 1); count++} END {if (count > 0) print int(sum/count); else print "0"}' "$1"
 }
