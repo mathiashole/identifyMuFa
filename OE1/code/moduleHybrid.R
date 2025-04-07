ed2cc89bb3c101297e3900631bd978f06394739e
mathiashole <mangino@fcien.edu.uy>
2025-04-07 15:13:06 -0300

    add: check file execution translated script

--- OE1/code/moduleHybrid.R
+++ OE1/code/moduleHybrid.R
@@ -123,9 +123,9 @@ execution_module_hybrid <- function(data, output_dir) {
         cat("Processing ALLBLAST: ", data$allblast_first_transeq_command[i], "\n")
         system(data$allblast_first_transeq_command[i])
         # Check if ALLBLAST created the expected file
-        path_file_first_translated <- file.path(output_dir, data$out_gscissors_high_translated[i])
+        path_file_first_translated <- file.path(output_dir, data$out_sequence_translated[i])
         if (!file.exists(path_file_first_translated)) {
-          cat("Error: ALLBLAST did not create the file", file.path(output_dir, data$out_gscissors_high_translated[i]), "\n")
+          cat("Error: ALLBLAST did not create the file", file.path(output_dir, data$out_sequence_translated[i]), "\n")
           next
         }
 
