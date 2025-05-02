2f26f14c12239c554cbfab564b19d618b666293d
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2025-05-02 16:48:41 -0300

    add: self sequence filter

--- OE1/code/bRefiner.sh
+++ OE1/code/bRefiner.sh
@@ -48,7 +48,7 @@ if [ "$UNIQ_SORT" = true ]; then
 else
     awk -v id_min="$IDENTITY_MIN" -v len_min="$LENGTH_MIN" -v evalue_max="$EVALUE_MAX" -v col="$COLUMN_TO_PRINT" '
     BEGIN { FS=OFS="\t" }
-    ($3 >= id_min) && ($4 >= len_min) && ($11 <= evalue_max) {
+    ($3 >= id_min) && ($4 >= len_min) && ($11 <= evalue_max) && (self_filter == "false" || $1 != $2) {
         if (col == "ALL") print $0;
         else print $col;
     }' "$BLAST_FILE" > "$OUTPUT_FILE"
