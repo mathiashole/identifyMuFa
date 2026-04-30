5725ce4145d5e6901351a828d11c672a66ccdbfc
mathiashole <mangino@fcien.edu.uy>
2026-04-30 09:22:47 -0300

    Update logic to extract best matches

--- OE2/code/bestMatch.sh
+++ OE2/code/bestMatch.sh
@@ -35,3 +35,9 @@ if [[ -z "$PREFIJO" ]]; then
     PREFIJO="${PREFIJO%.fna}"
     PREFIJO="${PREFIJO%.txt}"
 fi
+
+# --- Logic to extract best matches ---
+# 1. sort -k${COL},${COL}: gruping by the chosen ID column to ensure all entries for the same ID are together.
+# 2. -k3,3nr -k4,4nr: sort by identity (column 3) and then by alignment length (column 4) in descending order, so the best match is first for each ID.
+# 3. awk '!seen[$COL]++': Keep only the first occurrence of each unique ID in the chosen column, which corresponds to the best match due to the sorting.
+# 4. awk final: Extract the relevant columns for coordinates. If filtering by Subject (2), we take Subject ID (2) and coordinates (9 and 10). If filtering by Query (1), we take Query ID (1) and coordinates (7 and 8).
