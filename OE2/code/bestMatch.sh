281f348488a0b842449d3dfafb0b42e5ee0ba436
mathiashole <mangino@fcien.edu.uy>
2026-05-07 18:02:30 -0300

    Update usage help

--- OE2/code/bestMatch.sh
+++ OE2/code/bestMatch.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # --- Help / usage
+usage() {
+    echo "Usage: $0 -f <blast_file> [-c <colum 1|2>] [-o <output_file>]"
+    echo "  -f: Input file in BLAST tabular format (outfmt 6)."
+    echo "  -c: Column to use for best match (1 for query, 2 for subject). Default is 1 (query)."
+    echo "  -o: Prefix for output file. Default is 'best_matches'."
+    exit 1
+}
