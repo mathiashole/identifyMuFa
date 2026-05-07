6e4069b96a5f220fd902012f11d827854f4aa3bc
mathiashole <mangino@fcien.edu.uy>
2026-05-07 18:04:08 -0300

    Update default value

--- OE2/code/bestMatch.sh
+++ OE2/code/bestMatch.sh
@@ -4,9 +4,12 @@
 usage() {
     echo "Usage: $0 -f <blast_file> [-c <colum 1|2>] [-o <output_file>]"
     echo "  -f: Input file in BLAST tabular format (outfmt 6)."
-    echo "  -c: Column to use for best match (1 for query, 2 for subject). Default is 1 (query)."
+    echo "  -c: Column to use for best match (1 | 2). Default is 2."
     echo "  -o: Prefix for output file. Default is 'best_matches'."
     exit 1
 }
+
+# default values
+COL=2
 
 # --- Parse arguments
