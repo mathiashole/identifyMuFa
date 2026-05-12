0c9ea5e51d30a29858d202fbdc44d8d8224a6b3a
mathiashole <mangino@fcien.edu.uy>
2026-05-12 17:42:33 -0300

    Update frequency tm

--- OE2/keyCompare.R
+++ OE2/keyCompare.R
@@ -136,3 +136,8 @@ freq_only_rib <- only_rib_df %>%
 
 # IDs with only tm domains
 only_tm_df <- anti_join(tm_df, rib_df, by = c("file", "target_name"))
+
+# Frequency only have tm domains
+freq_only_tm <- only_tm_df %>%
+  group_by(file, target_name) %>%
+  summarise(count = n(), .groups = "drop")
