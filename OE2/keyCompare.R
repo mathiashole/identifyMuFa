ccf36a9d86ddf25413f6c8a38523c2b0a760cbc6
mathiashole <mangino@fcien.edu.uy>
2026-05-12 17:41:22 -0300

    Update frequency rib space

--- OE2/keyCompare.R
+++ OE2/keyCompare.R
@@ -131,5 +131,5 @@ only_rib_df <- anti_join(rib_df, tm_df, by = c("file", "target_name"))
 
 # Frequency only have rib domains
 freq_only_rib <- only_rib_df %>%
-  group_by(file, target_name) %>%
-  summarise(count = n(), .groups = "drop")
+    group_by(file, target_name) %>%
+    summarise(count = n(), .groups = "drop")
