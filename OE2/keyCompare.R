daa857fe10ddc4b754d328aff037f859bdf25ecc
mathiashole <mangino@fcien.edu.uy>
2026-05-12 15:23:26 -0300

    Update read tm and rename columns

--- OE2/keyCompare.R
+++ OE2/keyCompare.R
@@ -101,3 +101,6 @@ rib_df <- read_tsv(opts$rib_file, col_types = cols()) %>%
     rename(start_rib = start, end_rib = end, evalue_rib = e_value, tlen = tlen)
 
 rib_df <- filter(rib_df, (end_rib - start_rib) >= mean(tlen)*0.4)
+
+tm_df <- read_tsv(opts$tm_file, col_types = cols()) %>%
+  rename(start_tm = start, end_tm = end, evalue_tm = e_value, tlen = tlen)
